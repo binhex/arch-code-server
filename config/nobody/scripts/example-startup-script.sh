080f355791d45cad87bc63beaae4db8ca0e34397
binhex <megalith01@gmail.com>
2021-07-23 13:58:53 +0100

    more comments to startup script

--- config/nobody/scripts/example-startup-script.sh
+++ config/nobody/scripts/example-startup-script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Example script showing how to install packages from AOR/AUR.
+
 # aor packages
 ###
 
-# Define AOR (Arch Official Repository) packages
+# Define AOR (Arch Official Repository) packages you want to install at startup.
+# Go to the following URL for a searchable list of packages:- https://archlinux.org/packages/
 pacman_packages=""
 
 # install compiled packages using pacman
@@ -14,7 +17,8 @@ fi
 # aur packages
 ###
 
-# Define AUR (Arch User Repository) packages
+# Define AUR (Arch User Repository) packages you want to install at startup.
+# Go to the following URL for a searchable list of packages:- https://aur.archlinux.org/packages/
 aur_packages=""
 
 # install compiled packages using helper 'yay'
