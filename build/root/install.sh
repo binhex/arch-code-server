11ddf631d153ab8043a2f6106e16340f1316ddeb
binhex <megalith01@gmail.com>
2021-07-26 10:28:44 +0100

    move container perms to botton, remove dead symlink, done in gui

--- build/root/install.sh
+++ build/root/install.sh
@@ -58,61 +58,6 @@ source aur.sh
 cp -f '/home/nobody/icons/favicon'* '/usr/lib/code-server/src/browser/media/'
 cp -f '/usr/lib/code-server/src/browser/media/favicon.svg' '/usr/lib/code-server/src/browser/media/favicon-dark-support.svg'
 
-# container perms
-####
-
-# define comma separated list of paths
-install_paths="/home/nobody"
-
-# split comma separated string into list for install paths
-IFS=',' read -ra install_paths_list <<< "${install_paths}"
-
-# process install paths in the list
-for i in "${install_paths_list[@]}"; do
-
-	# confirm path(s) exist, if not then exit
-	if [[ ! -d "${i}" ]]; then
-		echo "[crit] Path '${i}' does not exist, exiting build process..." ; exit 1
-	fi
-
-done
-
-# convert comma separated string of install paths to space separated, required for chmod/chown processing
-install_paths=$(echo "${install_paths}" | tr ',' ' ')
-
-# set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
-chmod -R 775 "${install_paths}"
-
-# create file with contents of here doc, note EOF is NOT quoted to allow us to expand current variable 'install_paths'
-# we use escaping to prevent variable expansion for PUID and PGID, as we want these expanded at runtime of init.sh
-cat <<EOF > /tmp/permissions_heredoc
-
-# get previous puid/pgid (if first run then will be empty string)
-previous_puid=\$(cat "/root/puid" 2>/dev/null || true)
-previous_pgid=\$(cat "/root/pgid" 2>/dev/null || true)
-
-# if first run (no puid or pgid files in /tmp) or the PUID or PGID env vars are different
-# from the previous run then re-apply chown with current PUID and PGID values.
-if [[ ! -f "/root/puid" || ! -f "/root/pgid" || "\${previous_puid}" != "\${PUID}" || "\${previous_pgid}" != "\${PGID}" ]]; then
-
-	# set permissions inside container - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
-	chown -R "\${PUID}":"\${PGID}" ${install_paths}
-
-fi
-
-# write out current PUID and PGID to files in /root (used to compare on next run)
-echo "\${PUID}" > /root/puid
-echo "\${PGID}" > /root/pgid
-
-EOF
-
-# replace permissions placeholder string with contents of file (here doc)
-sed -i '/# PERMISSIONS_PLACEHOLDER/{
-    s/# PERMISSIONS_PLACEHOLDER//g
-    r /tmp/permissions_heredoc
-}' /usr/local/bin/init.sh
-rm /tmp/permissions_heredoc
-
 # env vars
 ####
 
@@ -212,9 +157,6 @@ if [[ "${ENABLE_STARTUP_SCRIPTS}" == "yes" ]]; then
 	chown -R nobody:users "${user_script_path}"
 
 fi
-
-# call symlink function from utils.sh
-symlink --src-path '/home/nobody' --dst-path '/config/home' --link-type 'softlink' --log-level 'WARN'
 EOF
 
 # replace config placeholder string with contents of file (here doc)
@@ -223,6 +165,61 @@ sed -i '/# CONFIG_PLACEHOLDER/{
     r /tmp/config_heredoc
 }' /usr/local/bin/init.sh
 rm /tmp/config_heredoc
+
+# container perms
+####
+
+# define comma separated list of paths
+install_paths="/home/nobody"
+
+# split comma separated string into list for install paths
+IFS=',' read -ra install_paths_list <<< "${install_paths}"
+
+# process install paths in the list
+for i in "${install_paths_list[@]}"; do
+
+	# confirm path(s) exist, if not then exit
+	if [[ ! -d "${i}" ]]; then
+		echo "[crit] Path '${i}' does not exist, exiting build process..." ; exit 1
+	fi
+
+done
+
+# convert comma separated string of install paths to space separated, required for chmod/chown processing
+install_paths=$(echo "${install_paths}" | tr ',' ' ')
+
+# set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+chmod -R 775 "${install_paths}"
+
+# create file with contents of here doc, note EOF is NOT quoted to allow us to expand current variable 'install_paths'
+# we use escaping to prevent variable expansion for PUID and PGID, as we want these expanded at runtime of init.sh
+cat <<EOF > /tmp/permissions_heredoc
+
+# get previous puid/pgid (if first run then will be empty string)
+previous_puid=\$(cat "/root/puid" 2>/dev/null || true)
+previous_pgid=\$(cat "/root/pgid" 2>/dev/null || true)
+
+# if first run (no puid or pgid files in /tmp) or the PUID or PGID env vars are different
+# from the previous run then re-apply chown with current PUID and PGID values.
+if [[ ! -f "/root/puid" || ! -f "/root/pgid" || "\${previous_puid}" != "\${PUID}" || "\${previous_pgid}" != "\${PGID}" ]]; then
+
+	# set permissions inside container - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+	chown -R "\${PUID}":"\${PGID}" ${install_paths}
+
+fi
+
+# write out current PUID and PGID to files in /root (used to compare on next run)
+echo "\${PUID}" > /root/puid
+echo "\${PGID}" > /root/pgid
+
+EOF
+
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+    s/# PERMISSIONS_PLACEHOLDER//g
+    r /tmp/permissions_heredoc
+}' /usr/local/bin/init.sh
+rm /tmp/permissions_heredoc
 
 # cleanup
 cleanup.sh
